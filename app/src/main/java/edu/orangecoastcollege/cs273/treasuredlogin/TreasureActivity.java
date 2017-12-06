1421825934092aabe876ccc6770216e6bef4180b
Severiano Badajoz <sevebadajoz@gmail.com>
2017-12-05 16:07:26 -0800

    Add code to grab email and add it to TextView, also make sign out button functional

--- app/src/main/java/edu/orangecoastcollege/cs273/treasuredlogin/TreasureActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/treasuredlogin/TreasureActivity.java
@@ -1,12 +1,18 @@
 package edu.orangecoastcollege.cs273.treasuredlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TreasureActivity extends AppCompatActivity {
 
     //TODO (1): Add Firebase member variables (auth and user)
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,21 @@ public class TreasureActivity extends AppCompatActivity {
 
 
         //TODO (2): Initialize Firebase authentication
+        mAuth = FirebaseAuth.getInstance();
         //TODO (3): Initialize current user
-
+        mUser = mAuth.getCurrentUser();
         TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
         //TODO (4): Set the text view's text to "Welcome " + user's email address
+        welcomeTextView.setText(getString(R.string.welcome_message, mUser.getEmail()));
 
     }
 
 
     // TODO (5): Create a public void handleSignOut(View v) that signs out of Firebase authentication,
     // TODO (5): finishes this activity and starts a new Intent back to the LoginActivity.
+    public void handleSignOut(View view) {
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
 }
